a79db5c9f8be743720ce05d9aa288f18e9a524d6
Mohd Hafiz Hilman <hilman_94@icloud.com>
2020-05-13 00:01:22 +0900

    Added image extract function.

--- extract_image.py
+++ extract_image.py
@@ -15,27 +15,31 @@ parser.add_argument(
 args = vars(parser.parse_args())
 
 def main():
-    for data_id in Path(args["input"]).iterdir():
-        video_file = list(data_id.rglob("*.mp4"))
-        csv_file = list(data_id.rglob("annotations.csv"))
-        if len(video_file) != 1 or len(csv_file) != 1:
+    # for data_id in Path(args["input"]).iterdir():
+    for csv_file in Path(args["input"]).rglob("annotations.csv"):
+        data_dir = csv_file.parent.parent / "data"
+        video_file = list(data_dir.rglob("*.mp4"))
+        if len(video_file) != 1:
             print(f"Skipping {data_id}. Probably problematic")
             continue
 
         video_file = video_file[0]
-        csv_file = csv_file[0]
-        images = extract_data(video_file, csv_file)
-        extracted_dir = video_file.parent / ".extracted"
+        try:
+            images = extract_data(video_file, csv_file)
+        except pd.errors.EmptyDataError:
+            continue
+
+        extracted_dir = data_dir / ".extracted"
         if extracted_dir.exists():
             rmtree(extracted_dir)
         extracted_dir.mkdir()
 
         save_images(images, extracted_dir)
 
-        print("Finished!")
+    print("Finished!")
 
 
-def extract_data(video_file, csv_file):
+def extract_data(video_file, csv_file) -> dict:
     video = cv2.VideoCapture(str(video_file))
     csv = pd.read_csv(csv_file)
 
